eb2d7666cdfdbe3ffb025b83f6415bc19628ca81
smfssilva <smfssilva@yahoo.com>
2021-04-20 20:54:45 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+# TODO: Passo a passo da solução.
+
+# TODO: Abrir os 6 arquivos do excel.
+
+# TODO: Para cada arquivo:
+
+# TODO: ParVerificar se algum valor na coluna vendas daquele arquivo é maior que 55.000
+
+# TODO: Se for maior que 55.000 -> enviar um sms com o Nome, o mês e as vendas do vendedor.
+
+# TODO: Caso não seja maior que 55.000 não quero fazer nada.
